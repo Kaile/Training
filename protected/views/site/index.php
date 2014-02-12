3dc62df36558890da11944eacc5a88d88215f4af
Mihail Kornilov <fix-06@yandex.ru>
2014-02-13 00:14:12 +0400

    Change the view

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -4,26 +4,34 @@
 $this->pageTitle=Yii::app()->name;
 ?>
 
-<h1 style="text-align: center">Список мероприятий</h1>
 <p>
-    <div id="forAdd" class="unit">
-        <span class="unitText">
-            <input value="Введите сюда свое новое мероприятие" type="text" id="textAdd" />
-        </span>
-        <span class="unitNumber">
-            <input value="1" type="text" id="numAdd" />
-        </span>
-        <span class="unitType">
-            <select class="selectType">
-                <option disabled selected>Выбрать единицу</option>
-                <option value="hour">Часы</option>
-                <option value="unit">Штуки</option>
-                <option value="bout">Чвсы</option>
-            </select>
-        </span>
-        <span class="btnAdd">
-            <input type="button" value="Добавить" />
-        </span>
-    </div>
+    <?php if (!empty($statistic)):?>
+        <h2 style="text-align: center">Список мероприятий</h2>
+        <div class="unit">
 
+        </div>
+        <hr >
+    <?php endif?>
+    <h2 style="text-align: center">Добавить новое мероприятие</h2>
+    <form name="addUnit" action="/site/index" method="post">
+        <div id="forAdd" class="unit">
+            <span class="unitText">
+                <input value="Введите сюда свое новое мероприятие" type="text" id="textAdd" />
+            </span>
+            <span class="unitNumber">
+                <input value="1" type="text" id="numAdd" />
+            </span>
+            <span class="unitType">
+                <select class="selectType">
+                    <option disabled selected>Выбрать единицу</option>
+                    <option value="hour">Часы</option>
+                    <option value="unit">Штуки</option>
+                    <option value="bout">Чвсы</option>
+                </select>
+            </span>
+            <span class="btnAdd">
+                <input type="button" value="Добавить" />
+            </span>
+        </div>
+    </form>
 </p>
